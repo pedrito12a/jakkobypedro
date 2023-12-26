d9d188e1f2f4b57b8de532e7a00f3d15f164aa8f
pedrito12a <153040660+pedrito12a@users.noreply.github.com>
2023-12-26 02:51:03 +0100

    Update pruebaNotepad.ps1

--- pruebaNotepad.ps1
+++ pruebaNotepad.ps1
@@ -1,10 +1,6 @@
 Start-Process 'C:\windows\system32\notepad.exe' -Wait
 
 $wshell = New-Object -ComObject wscript.shell;
-$wshell.AppActivate('Sin título: Bloc de notas')
-$wshell.sendkeys("Hola")
-Start-Sleep -Seconds 2
-$wshell.sendkeys("^a")
 Start-Sleep -Seconds 2
 $wshell.sendkeys("^s")
 Start-Sleep -Seconds 2
